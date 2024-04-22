9e5ed9a4690601988b69ec3a0dd8d8f1ac0f1525
Tobi Idowu <idtobi8@gmail.com>
2024-04-22 17:30:40 +0100

    Updated Farmofftake page

---
+++ app/shop/route-display.tsx
@@ -0,0 +1,27 @@
+import { ShoppingBag, ChevronRight } from 'lucide-react';
+import React from 'react';
+
+import { Text } from '@/components/ui/text';
+
+function RouteDisplay({ route }: { route: string }) {
+  return (
+    <div
+      style={{
+        backgroundImage: `url(/images/shop/route-display-bg.png)`,
+        backgroundRepeat: 'no-repeat',
+        backgroundSize: 'cover',
+      }}
+      className="flex w-full items-center justify-between bg-black p-8"
+    >
+      <div className="flex items-center justify-start gap-2">
+        <ShoppingBag className="w-6 text-white" />
+        <ChevronRight className="w-6 text-white" />
+        <Text className="text-primary-2" weight={'medium'} size={'sm'}>
+          {route}
+        </Text>
+      </div>
+    </div>
+  );
+}
+
+export default RouteDisplay;
