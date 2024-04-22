950510c9e433bd6910cef2c3648e4f5423a05777
Tobi Idowu <idtobi8@gmail.com>
2024-04-22 19:34:43 +0100

    Updated price.tsx

--- app/shop/categories/molecules/price.tsx
+++ app/shop/categories/molecules/price.tsx
@@ -6,7 +6,7 @@ import { Text } from '@/components/ui/text';
 
 function price() {
   return (
-    <AccordionItem className="border-0" value="item-1">
+    <AccordionItem className="border-0" value="item-2">
       <AccordionTrigger>Price</AccordionTrigger>
       <AccordionContent className="flex items-center justify-start gap-1">
         {/* <Input className="w-3" type="radio" placeholder="Email" /> */}
