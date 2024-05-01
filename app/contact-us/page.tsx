dbea6e2ce692fda3506c9609f69805e72f0d2c80
giftthedeveloper <giftjeremiah001@gmail.com>
2024-05-01 14:20:30 +0100

    feat: created header text and subtext

--- app/contact-us/page.tsx
+++ app/contact-us/page.tsx
@@ -3,6 +3,8 @@ import React from 'react';
 import useWindowDimensions from '@/hooks/useWindowDimensions';
 import Container from '@/components/shared/container';
 import RouteDisplay from '../shop/route-display';
+import HeaderText from '@/components/ui/header-text';
+import { Text } from '@/components/ui/text';
 
 function page() {
   // eslint-disable-next-line react-hooks/rules-of-hooks
@@ -12,7 +14,14 @@ function page() {
       {width && width > 768 && <RouteDisplay route={'Contact Us'} />}
 
       <Container>
-      <main style={{ maxWidth: '1200px' }} className="mx-auto mt-8 flex w-full flex-col items-center justify-center gap-1 py-4">
+        <main style={{ maxWidth: '1200px' }} className="mx-auto mt-8 flex w-full flex-col items-center justify-center gap-1 py-4">
+            <HeaderText text="Reach Out to Us" color="black" position="left" />
+            <Text
+            className="text-center text-gray-500" 
+            size="sm"
+            weight="normal">
+            Have any questions, suggestions, advice complaints or inquires? Feel free to contact us using any <br/>of the options below!
+            </Text>
 
         </main>
       </Container>
---
+++ components/ui/header-text.tsx
@@ -0,0 +1,21 @@
+
+interface HeaderProps {
+    text: string;
+    color?: string;
+    position?: 'left' | 'center' | 'right'; 
+  }
+
+function HeaderText({ text, color, position }: HeaderProps) {
+    const headerStyle = {
+      textAlign: position || 'center',
+      padding: '1rem 0',
+      color: color || '#000',
+    };
+  
+    return (
+      <header style={headerStyle}>
+        <h1 className="text-6xl font-bold">{text}</h1>
+      </header>
+    );
+  }
+export default HeaderText;  
