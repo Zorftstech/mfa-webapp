b800c19ac2ef34afdcb26c2237400bb929a17b81
Tobi Idowu <idtobi8@gmail.com>
2024-05-03 11:35:04 +0100

    Update sidebar & stickybar

--- app/dashboard/molecules/stickybar.tsx
+++ app/dashboard/molecules/stickybar.tsx
@@ -7,24 +7,25 @@ import Link from "next/link";
 import { usePathname } from "next/navigation";
 import { Button } from "@/components/ui/button";
 
-function Sidebar() {
+function Stickybar() {
    const pathname = usePathname();
    return (
-      <div className="flex flex-1 flex-col items-center justify-start gap-2 bg-white p-3">
+      <div className="flex flex-1 flex-col items-center justify-start gap-2 border border-blue-500 bg-white p-3">
          <Each
             of={dashboardRoutes}
             render={(route: any, index: number) => (
                <Link
-                  className={`w-full rounded-md border duration-200 hover:border-gray-100 hover:bg-gray-100 hover:text-[#7ab42c] ${pathname === route.href ? "border-gray-100 bg-gray-100 text-[#7ab42c]" : "border-transparent"} px-2 py-3 text-sm`}
+                  className={`w-full rounded-md border ${pathname === route.href ? "border-gray-100 bg-gray-100 text-[#7ab42c]" : "border-transparent"} px-2 py-3 text-sm`}
                   href={route.href}
                >
                   {route.title}
                </Link>
             )}
          />
-         <Button className="mt-5 w-full">Logout</Button>
+
+         <Button>Logout</Button>
       </div>
    );
 }
 
-export default Sidebar;
+export default Stickybar;
