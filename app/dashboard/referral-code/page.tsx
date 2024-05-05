09868ef35aa4f31c511106534cfb290ce1726761
giftthedeveloper <giftjeremiah001@gmail.com>
2024-05-05 18:29:47 +0100

    feat: added padddding to the referral code button

--- app/dashboard/referral-code/page.tsx
+++ app/dashboard/referral-code/page.tsx
@@ -11,7 +11,7 @@ function page() {
             <LineDivider width="95%" color="#DDDDDD" />
             <div className='py-3 px-5 '>
             <p style={{ fontWeight: '500' }} className="text-content mb-4 mt-2">Your referral code is</p>
-            <button style={{ backgroundColor: '#F7F7F7', color: '#7AB42C', fontSize: '1.5rem', fontWeight: '600' }} className="text-lg py-2 px-4 mb-10">AB720LSR</button>
+            <button style={{ backgroundColor: '#F7F7F7', color: '#7AB42C', fontSize: '1.5rem', fontWeight: '600' }} className="text-lg py-4 px-6 mb-10">AB720LSR</button>
 
             </div>
         </div>
