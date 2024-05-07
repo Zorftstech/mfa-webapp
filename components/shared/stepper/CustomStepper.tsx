ada861861610ac68a002671be5ee0cf666429ddf
Abdulhameed <busarioyewole@gmail.com>
2024-05-07 23:23:39 +0100

    Create stepper component

---
+++ components/shared/stepper/CustomStepper.tsx
@@ -0,0 +1,38 @@
+"use client";
+
+import { Icon } from "@iconify/react";
+import "./stepper.css";
+
+const CustomStepper = ({
+   steps,
+   activeStep,
+   isComplete,
+}: {
+   steps: any[];
+   activeStep: number;
+   isComplete?: boolean;
+}) => {
+   return (
+      <div className="flex flex-col justify-between md:flex-row">
+         {steps?.map((step, i) => (
+            <div
+               key={i}
+               className={`step-item ${activeStep === i + 1 && "active"} ${
+                  (i + 1 < activeStep || isComplete) && "complete"
+               } `}
+            >
+               <div className="step">
+                  {i + 1 < activeStep || isComplete ? (
+                     <Icon icon="fluent-mdl2:check-mark" width="24" height="24" />
+                  ) : (
+                     i + 1
+                  )}
+               </div>
+               <p className="text-gray-500">{step}</p>
+            </div>
+         ))}
+      </div>
+   );
+};
+
+export default CustomStepper;
