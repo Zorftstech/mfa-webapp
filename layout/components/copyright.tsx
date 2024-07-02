6b0d1bc69ee1700a23041e24bbe81682bf95f3ae
Tobi Idowu <97598133+Tobbs101@users.noreply.github.com>
2024-07-02 13:55:03 +0100

    Updated copyright

--- layout/components/copyright.tsx
+++ layout/components/copyright.tsx
@@ -1,31 +1,36 @@
-import React from 'react';
+import React from "react";
 
-import Image from 'next/image';
+import Image from "next/image";
 
-import Each from '@/components/helpers/each';
-import { Text } from '@/components/ui/text';
+import Each from "@/components/helpers/each";
+import { Text } from "@/components/ui/text";
 
-import flutterwave from '../../images/flutterwave.png';
-import mastercard from '../../images/mastercard.png';
-import paystack from '../../images/paystack.png';
-import visa from '../../images/visa.png';
+import flutterwave from "../../images/flutterwave.png";
+import mastercard from "../../images/mastercard.png";
+import paystack from "../../images/paystack.png";
+import visa from "../../images/visa.png";
 
 const images = [paystack, flutterwave, mastercard, visa];
 
 function Copyright() {
-  return (
-    <div className="mb-6 flex w-full flex-col items-center justify-between gap-2 px-4 pt-6 sm:flex-row">
-      <Text style={{ color: '#989898' }} size={'xs'} weight={'medium'}>
-        &copy; MY FOOD ANGELS.COM, All rights reserved.
-      </Text>
-      <div className="flex items-center justify-end gap-2">
-        <Each
-          of={images}
-          render={(item, index) => <Image key={index} className="h-10 w-10" src={item} alt="Partners" />}
-        />
+   return (
+      <div className="mb-6 flex w-full flex-col items-center justify-between gap-2 px-4 pt-6 sm:flex-row">
+         <Text style={{ color: "#989898" }} size={"xs"} weight={"medium"}>
+            &copy; MY FOOD ANGELS.COM, All rights reserved.
+         </Text>
+         <Text style={{ color: "#989898" }} size={"xs"} weight={"medium"}>
+            Designed & Developed by Zorfts Technologies Ltd.
+         </Text>
+         <div className="flex items-center justify-end gap-2">
+            <Each
+               of={images}
+               render={(item, index) => (
+                  <Image key={index} className="h-10 w-10" src={item} alt="Partners" />
+               )}
+            />
+         </div>
       </div>
-    </div>
-  );
+   );
 }
 
 export default Copyright;
