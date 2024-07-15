3bbacf2c856fe0b625683e57565686824972f68b
kela <ikwuhjefferye@gmail.com>
2024-07-15 16:29:36 +0100

    chore: Update VSCode settings and Firebase configuration
    Update VSCode settings to remove custom color customizations for the activity bar and title bar. Also, update the Firebase configuration in the irebase/index.ts file with new credentials. These changes improve the development environment and ensure the correct connection to the Firebase services.

--- firebase/index.ts
+++ firebase/index.ts
@@ -4,12 +4,12 @@ import { getStorage } from "firebase/storage";
 import { getFirestore } from "firebase/firestore";
 
 const firebaseConfig = {
-   apiKey: "AIzaSyBRQh0FdZo0Cq5F9ln0iWg1SnwjRqCkNWo",
-   authDomain: "myfoodangels-7711d.firebaseapp.com",
-   projectId: "myfoodangels-7711d",
-   storageBucket: "myfoodangels-7711d.appspot.com",
-   messagingSenderId: "49313223918",
-   appId: "1:49313223918:web:c61dff71c79bafcbd8cfff",
+    apiKey: "AIzaSyAI7ZmwJPhhtBUFEhxu9D8lOPsxKoySO2k",
+  authDomain: "mfa-1d165.firebaseapp.com",
+  projectId: "mfa-1d165",
+  storageBucket: "mfa-1d165.appspot.com",
+  messagingSenderId: "588241135226",
+  appId: "1:588241135226:web:5c5e36b6c6e4c8663cbdc3"
 };
 
 // Initialize Firebase
